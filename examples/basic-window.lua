d9e1bd342b4deff2683cf21ed21c32853b8ec556
deadhoes <noctalis.softworks@gmail.com>
2025-06-27 11:21:09 +0300

    Update basic-window.lua

--- examples/basic-window.lua
+++ examples/basic-window.lua
@@ -1,1 +1,37 @@
+local UI = loadstring(game:HttpGet("your-url-here"))()
 
+local Window = UI:CreateWindow({
+    Name = "Basic Window"
+})
+
+local MainTab = Window:CreateTab("Main")
+
+MainTab:CreateButton({
+    Name = "Hello World",
+    Callback = function()
+        print("Hello World!")
+    end
+})
+
+MainTab:CreateSlider({
+    Name = "Value",
+    Range = {1, 10},
+    CurrentValue = 5,
+    Callback = function(value)
+        print("New value:", value)
+    end
+})
+
+MainTab:CreateDropdown({
+    CurrentOption = {"One"},
+    Options = {"One", "Two", "Three"},
+    Callback = function(option)
+        print("Selected:", option)
+    end
+})
+
+UI:Notify({
+    Title = "Started",
+    Content = "Basic window is ready!",
+    Duration = 3
+})
